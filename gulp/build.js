351743cc2a7a2b17a519c58d36c54e13063ad47c
王青 <ericwangqing@gmail.com>
2016-02-08 09:18:00 +0800

    修复bug

--- gulp/build.js
+++ gulp/build.js
@@ -11,7 +11,7 @@ var $ = require('gulp-load-plugins')({
 
 gulp.task('partials', function ()
 {
-    var _jade = config.jade();
+    var _jade = conf.jade();
     var _html = gulp.src([
             path.join(conf.paths.src, '/app/**/*.html'),
             path.join(conf.paths.tmp, '/serve/app/**/*.html')
